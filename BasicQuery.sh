40753cba4bb73e19f1c83446d639822a891de6f9
Omkar Kamble <66213270+Omkar4048@users.noreply.github.com>
2022-08-04 12:09:45 +0530

    Create BasicQuery.sh

---
+++ BasicQuery.sh
@@ -0,0 +1,2 @@
+#python script.py "Source-Ip" "Destination-Ip" "Protocol" "Port" "Rule-Name" "Rule-Description" "Scheduler" "Device-Group"
+python AutomationVer2.py "1.1.1.1,2.2.2.2" "3.3.3.3,4.4.4.4" "tcp" "80,22" "Test_Rule_Name" "RuleDescription" "30" "Test"
