b688af80d4124ab598fe46a7111c208aa2d044eb
Omkar Kamble <66213270+Omkar4048@users.noreply.github.com>
2022-08-04 11:47:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+#it is good idea to keep your authentication key private at your end and with different file that your Code.
+#we will call this key in our script.
+key = "<Add your Authentication Key here>"
